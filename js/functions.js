d7f53b6212e186276ff373cc6df7c97a299fc234
Spark4444 <102835794+Mista44444444@users.noreply.github.com>
2023-09-17 20:47:48 +0100

    Delete js directory

--- js/functions.js
+++
@@ -1,5 +0,0 @@
-// Save a key-value pair to local storage
-const saveToLocalStorage = (key, value) => localStorage.setItem(key, value);
-
-// Retrieve a value from local storage by its key
-const getFromLocalStorage = key => localStorage.getItem(key);
