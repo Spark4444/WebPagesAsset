2f0f1c8cbae9a80a5df70ed4940250d7ab4a5382
Spark4444 <102835794+Mista44444444@users.noreply.github.com>
2023-09-27 15:07:36 +0200

    Update functions.js

--- Asset/js/functions.js
+++ Asset/js/functions.js
@@ -3,3 +3,8 @@ const saveToLocalStorage = (key, value) => localStorage.setItem(key, value);
 
 // Retrieve a value from local storage by its key
 const getFromLocalStorage = key => localStorage.getItem(key);
+
+// Function to generate a random number between a start and end value
+function getRandomNumber(start, end) {
+    return Math.floor(Math.random() * (end - start + 1)) + start;
+}
